191989978e5df90961ccece94065c124218bb70e
Gabriel Araujo <gabrielrdearaujo1023@hotmail.com>
2024-05-01 21:55:50 -0300

    Update authentication to the sidebar

--- src/App.js
+++ src/App.js
@@ -2,32 +2,51 @@ import { useState} from 'react'
 import { MoviesPage } from './components/MoviesPage'
 import { MovieListPage } from './components/MovieListPage'
 import './App.css'
-import Login from './components/Login'
+import { auth, googleProvider } from './config/keysConfig'
+import { signInWithPopup, signOut } from 'firebase/auth'
 
 const App = () => {
   var [page, setPage] = useState('search');
-  var [isAuth, setIsAuth] = useState(false);
-  console.log(page);
+  var [currentUser, setCurrentUser] = useState(auth.currentUser?.uid);
+
+  async function Login () {
+    try {
+        await signInWithPopup(auth, googleProvider)
+        .then(setCurrentUser(auth.currentUser?.uid));
+    } catch (e) {
+        console.error(e);
+    }
+  }
+
+  async function Logout () {
+    try {
+      await signOut(auth)
+      .then(setCurrentUser(null));
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   return (
     <>
     <div className='app'>
-      {isAuth ?
-        <>
-        <div className="sideMenu">
-          <button onClick={() => setPage('search')} className='menuButton'> Search Movies</button>
-          <br/><br/>
-          <button onClick={() => setPage('list')} className='menuButton'> Movie List</button>
-        </div>
-        <div className="content">
+      <div className="sideMenu">
+        {currentUser !== null ? 
+          <button onClick={() => {Logout()}}>Log Out</button> : 
+          <button onClick={() => {Login()}}>Log In With Google</button>
+        }
+        
+        <br/><br/>
+        <button onClick={() => setPage('search')} className='menuButton'> Search Movies</button>
+        <br/><br/>
+        <button onClick={() => setPage('list')} className='menuButton'> Movie List</button>
+      </div>
+      <div className="content">
       
-          <h1>MovieFlix</h1>
-          {page === 'search' && ( <MoviesPage />)}
-          {page === 'list' && ( <MovieListPage />)}
-        </div>
-        </> :
-        <Login setIsAuth={setIsAuth}/>
-      }
+        <h1>MovieFlix</h1>
+        {page === 'search' && ( <MoviesPage />)}
+        {page === 'list' && ( <MovieListPage />)}
+      </div>
     </div>
     </>
   )
